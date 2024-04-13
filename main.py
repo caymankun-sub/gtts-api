009ba807fb59440d82f3ebefce94574e0fa0d137
caymankun-sub <165893481+caymankun-sub@users.noreply.github.com>
2024-04-13 10:00:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, Response
 from gtts import gTTS
 import base64
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['POST'])
 def synthesize_text():
